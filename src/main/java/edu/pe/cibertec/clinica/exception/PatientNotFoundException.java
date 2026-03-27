1306e9c4a6358c99919cffa5262b380cab5f8929
IJuanDiego <i202336416@cibertec.edu.pe>
2026-03-27 13:37:14 -0500

    api patients completed

---
+++ src/main/java/edu/pe/cibertec/clinica/exception/PatientNotFoundException.java
@@ -0,0 +1,8 @@
+package edu.pe.cibertec.clinica.exception;
+
+public class PatientNotFoundException extends RuntimeException {
+    public PatientNotFoundException(Long id) {
+
+        super("Patient not found with id: " + id);
+    }
+}
